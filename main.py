adf00ac55bafa5a7500f2df834b3257de88e031e
ngripon <nathan.gripon@gmail.com>
2024-08-24 16:33:04 +0200

    add magic methods

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from typing import Callable
 
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
 
 class Solver:
     def __init__(self):
@@ -7,16 +10,18 @@ class Solver:
         self.vars = []
         self.feed_vars = []
 
-    def create_variables(self, x0: list) -> list:
+    def create_variables(self, x0: tuple) -> list:
         n = len(x0)
-        var0 = FeedVar()
-        var_list = [var0]
+        var_list = []
+        var = TemporalVar(lambda t, y: y[self.dim])
+        var.set_init(x0[-1])
+        var_list.append(var)
         for i in range(n):
             if i != n - 1:
-                var = FeedVar(lambda t, y: y[self.dim + n - i])
+                var = FeedVar(lambda t, y: y[self.dim + i + 1])
+                var.set_init(x0[-2 - i])
             else:
-                var = TemporalVar(lambda t, y: y[self.dim])
-            var.set_init(x0[i])
+                var = FeedVar()
             var_list.append(var)
         self.dim += n
         return var_list
@@ -36,6 +41,36 @@ class TemporalVar:
     def set_init(self, x0: float):
         self.content.append(x0)
 
+    def __add__(self, other):
+        if isinstance(other, TemporalVar):
+            return TemporalVar(lambda t, y: self.function(t, y) + other.function(t, y))
+        else:
+            return TemporalVar(lambda t, y: other + self.function(t, y))
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, TemporalVar):
+            return TemporalVar(lambda t, y: self.function(t, y) - other.function(t, y))
+        else:
+            return TemporalVar(lambda t, y: self.function(t, y) - other)
+
+    def __rsub__(self, other):
+        if isinstance(other, TemporalVar):
+            return TemporalVar(lambda t, y: other.function(t, y) - self.function(t, y))
+        else:
+            return TemporalVar(lambda t, y: other - self.function(t, y))
+
+    def __mul__(self, other):
+        if isinstance(other, TemporalVar):
+            return TemporalVar(lambda t, y: self.function(t, y) * other.function(t, y))
+        else:
+            return TemporalVar(lambda t, y: other * self.function(t, y))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
     def __repr__(self):
         return f"{self.content=}"
 
@@ -43,6 +78,7 @@ class TemporalVar:
 class FeedVar(TemporalVar):
     def __init__(self, fun: Callable = None):
         super().__init__(fun)
+        self.solver.feed_vars.append(self)
 
     def set_value(self, value):
         if isinstance(value, TemporalVar):
@@ -51,7 +87,23 @@ class FeedVar(TemporalVar):
             self.function = lambda t, y: value
 
 
+def mass_spring_damper(t, y):
+    ddy = 1 / m * (-c * y[1] - k * y[0])
+    return y[1], ddy
+
+
+t_final = 50
 m = 5
 k = 1
 c = 0.5
-acc, vit, pos = solver.create_variables([0, 1])
+v0 = 0
+x0 = 1
+# Comparison
+res_normal = solve_ivp(mass_spring_damper, [0, t_final], (x0, v0))
+print(res_normal)
+
+pos, vit, acc = solver.create_variables((x0, v0))
+acc.set_value(1 / m * (-c * vit - k * pos))
+
+plt.plot(res_normal.t, res_normal.y[0])
+plt.show()
