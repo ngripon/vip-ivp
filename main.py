e5a3569567733fc4295f10de6db4e77958465566
ngripon <nathan.gripon@gmail.com>
2024-08-25 16:23:56 +0200

    add lazy computation

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class Solver:
         self.vars = []
         self.feed_vars = []
         self.initialized_vars = []
+        self.t = None
+        self.y = None
+        self.solved = False
 
     def create_variables(self, x0: tuple) -> list:
         n = len(x0)
@@ -20,8 +23,8 @@ class Solver:
         var_list.append(var)
         for i in range(n):
             if i != n - 1:
-                var = FeedVar(lambda t, y, idx=self.dim+i+1: y[idx])
-                var.set_init(x0[1+i])
+                var = FeedVar(lambda t, y, idx=self.dim + i + 1: y[idx])
+                var.set_init(x0[1 + i])
             else:
                 var = FeedVar()
             var_list.append(var)
@@ -29,10 +32,13 @@ class Solver:
         return var_list
 
     def solve(self, t_end: float):
-        x0 = [x.content[0] for x in self.initialized_vars]
-        start=time.time()
+        x0 = [x.init for x in self.initialized_vars]
+        start = time.time()
         res = solve_ivp(self._dy, (0, t_end), x0)
-        print(f"Performance = {time.time()-start}")
+        print(f"Performance = {time.time() - start}")
+        self.t = res.t
+        self.y = res.y
+        self.solved = True
         return res
 
     def _dy(self, t, y):
@@ -48,14 +54,24 @@ solver = Solver()
 class TemporalVar:
     def __init__(self, fun: Callable = None):
         self.solver = solver
-        self.content = []
+        self.init = None
         self.function = fun
         self.initialized = False
+        self._values = None
 
         self.solver.vars.append(self)
 
+    @property
+    def values(self):
+        if not self.solver.solved:
+            raise Exception("The differential system has not been solved. "
+                            "Call the solve() method before inquiring the variable values.")
+        if self._values is None:
+            self._values = self.function(self.solver.t, self.solver.y)
+        return self._values
+
     def set_init(self, x0: float):
-        self.content.append(x0)
+        self.init=x0
         self.initialized = True
         self.solver.initialized_vars.append(self)
 
@@ -93,7 +109,7 @@ class TemporalVar:
         return TemporalVar(lambda t, y: - self.function(t, y))
 
     def __repr__(self):
-        return f"{self.content=}"
+        return f"{self.values}"
 
 
 class FeedVar(TemporalVar):
@@ -120,18 +136,18 @@ c = 0.5
 v0 = 2
 x0 = 5
 # Comparison
-start=time.time()
+start = time.time()
 res_normal = solve_ivp(mass_spring_damper, [0, t_final], (x0, v0))
-print(time.time()-start)
+print(time.time() - start)
 # print(res_normal)
 
 pos, vit, acc = solver.create_variables((x0, v0))
 acc.set_value(1 / m * (-c * vit - k * pos))
-
+my_test=2*acc
 
 res_awesome = solver.solve(t_final)
 
-
 # plt.plot(res_normal.t, res_normal.y[1])
-# plt.plot(res_awesome.t, res_awesome.y[1])
-# plt.show()
+plt.plot(solver.t, acc.values)
+plt.plot(solver.t, my_test.values)
+plt.show()
