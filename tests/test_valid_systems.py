8cb13f962258aaf11846afabcad4bfe6eb7ab314
ngripon <n.gripon@sherpa-eng.com>
2024-09-09 16:12:00 +0200

    small improvement to naming

--- tests/test_valid_systems.py
+++ tests/test_valid_systems.py
@@ -11,13 +11,13 @@ def solver():
 
 
 def test_operator_overloading(solver):
-    pos, vit, acc = solver.create_variables((0, 0))
+    pos, vit, acc = solver.create_derivatives((0, 0))
     acc.set_value(-pos * vit - pos / vit % vit // pos + abs(pos ** vit))
     solver.solve(10)
 
 
 def test_pendulum(solver):
-    th, d_th, dd_th = solver.create_variables((0, np.pi / 2))
+    th, d_th, dd_th = solver.create_derivatives((0, np.pi / 2))
     dd_th.set_value(-9.81 / 1 * np.sin(th))
     solver.solve(10)
     # plt.plot(th.t, th.values)
