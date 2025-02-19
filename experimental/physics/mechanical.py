eaa487b54d2e4b28229433acf083fe2547fc1e49
ngripon <n.gripon@sherpa-eng.com>
2025-02-19 17:41:18 +0100

    demo with class instead of function

--- experimental/physics/mechanical.py
+++ experimental/physics/mechanical.py
@@ -1,8 +1,9 @@
+from numbers import Number
 from typing import Union
 
 import matplotlib.pyplot as plt
 
-import main as vip
+import vip_ivp as vip
 
 
 class Mechanical1DBond:
@@ -43,8 +44,10 @@ class Mechanical1DBondFlow(Mechanical1DBond):
     def from_effort(cls, bond: "Mechanical1DBondEffort"):
         if isinstance(bond, Mechanical1DBondEffort):
             loop_node = vip.loop_node(bond.force)
-        else:
+        elif isinstance(bond, Number):
             loop_node = vip.loop_node(bond)
+        else:
+            raise Exception(f"Incompatible type: {bond} of type {type(bond)}.")
         new_bond = cls(0, loop_node)
         return new_bond, loop_node
 
@@ -65,8 +68,10 @@ class Mechanical1DBondEffort(Mechanical1DBond):
     def from_flow(cls, bond: "Mechanical1DBondFlow"):
         if isinstance(bond, Mechanical1DBondFlow):
             loop_node = vip.loop_node(bond.speed)
-        else:
+        elif isinstance(bond, Number):
             loop_node = vip.loop_node(bond)
+        else:
+            raise Exception(f"Incompatible type: {bond} of type {type(bond)}.")
         new_bond = cls(0, loop_node)
         return new_bond, loop_node
 
@@ -86,48 +91,45 @@ def set_flow(bond: Union[Mechanical1DBond, float], flow: vip.TemporalVar):
         bond.flow = flow
 
 
-def inertia(input_effort: Mechanical1DBondEffort, mass, gravity: bool, speed0: float = 0) -> Mechanical1DBondFlow:
-    output_flow, effort = Mechanical1DBondFlow.from_effort(input_effort)
-    acc = effort / mass
-    if gravity:
-        acc += 9.81
-    speed = vip.integrate(acc, speed0)
-    set_flow(output_flow, speed)
-    set_flow(input_effort, speed)
-    return output_flow
+class Inertia:
+    def __init__(self, port1: Union[Mechanical1DBondEffort, float], mass: float, gravity: bool, speed0: float = 0):
+        output_flow, effort = Mechanical1DBondFlow.from_effort(port1)
+        acc = effort / mass
+        if gravity:
+            acc += 9.81
+        speed = vip.integrate(acc, speed0)
+        set_flow(output_flow, speed)
+        set_flow(port1, speed)
+        self.port2 = output_flow
 
 
-def spring(input_flow: Mechanical1DBondFlow, stiffness: float, x0: float = 0) -> Mechanical1DBondEffort:
-    output_effort, flow = Mechanical1DBondEffort.from_flow(input_flow)
-    x = vip.integrate(flow, x0)
-    effort_value = stiffness * x
-    set_effort(input_flow, -effort_value)
-    set_effort(output_effort, effort_value)
-    return output_effort
+class Spring:
+    def __init__(self, port1: Union[Mechanical1DBondFlow, float], stiffness: float, x0: float = 0):
+        output_effort, flow = Mechanical1DBondEffort.from_flow(port1)
+        x = vip.integrate(flow, x0)
+        effort_value = stiffness * x
+        set_effort(port1, -effort_value)
+        set_effort(output_effort, effort_value)
+        self.port2 = output_effort
 
 
 if __name__ == '__main__':
-
-    # Double spring system
-    # mass_output = inertia(0, 1, 0, 1)
-    # spring_output = spring(mass_output, 1, 0)
-    # mass_2_output = inertia(spring_output, 1, 0)
-    # spring2_output=spring(mass_2_output, 1)
-
-    # # 100 spring system
+    # 100 spring system
     n_springs = 100
     mass_list = []
     spring_list = []
-    current = 0
+    current_effort = 0
     for i in range(n_springs):
-        mass_output = inertia(current, 1, False, 1 if i == 0 else 0)
-        current = spring(mass_output, 1)
-        mass_list.append(mass_output)
-        spring_list.append(current)
+        mass = Inertia(current_effort, 1, False, 1 if i == 0 else 0)
+        spring = Spring(mass.port2, 1)
+        current_effort = spring.port2
+
+        mass_list.append(mass.port2)
+        spring_list.append(spring.port2)
 
     vip.solve(500, time_step=0.01)
+    # Plot
 
     for mass in mass_list[:1]:
         plt.plot(mass.flow.t, mass.flow)
     plt.show()
-
