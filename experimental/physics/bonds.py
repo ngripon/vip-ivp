746f52e419a4c99b017dacced47460699356e3f8
ngripon <n.gripon@sherpa-eng.com>
2025-02-20 16:09:07 +0100

    erase metaprogramming as it is broken

--- experimental/physics/bonds.py
+++ experimental/physics/bonds.py
@@ -31,71 +31,49 @@ class Bond:
         return self.flow * self.effort
 
 
-def create_bond_types(name: str, effort_name: str, flow_name: str) -> tuple[Type["Bond"], Type["Bond"]]:
-    """Dynamically creates BondEffort and BondFlow classes with renamed attributes."""
+class BondFlow(Bond):
+    def __init__(self, value: float, loop_node):
+        super().__init__()
+        self.flow = value
+        self.effort = None
+        self.loop_node = loop_node
 
-    class BondEffort(Bond):
-        """Effort-driven bond."""
+    @classmethod
+    def from_effort(cls, bond: "BondEffort"):
+        if isinstance(bond, BondEffort):
+            loop_node = vip.loop_node(bond.effort)
+        elif isinstance(bond, Number):
+            loop_node = vip.loop_node(bond)
+        else:
+            raise Exception(f"Incompatible type: {bond} of type {type(bond)}.")
+        new_bond = cls(0, loop_node)
+        return new_bond, loop_node
 
-        def __init__(self, value: float, loop_node):
-            super().__init__()
-            setattr(self, flow_name, None)
-            setattr(self, effort_name, value)
-            self.loop_node = loop_node
-
-        @classmethod
-        def from_flow(cls, bond):
-            if isinstance(bond, BondFlow):
-                loop_node = vip.loop_node(getattr(bond, flow_name))
-            elif isinstance(bond, Number):
-                loop_node = vip.loop_node(bond)
-            else:
-                raise Exception(f"Incompatible type: {bond} of type {type(bond)}.")
-            new_bond = cls(0, loop_node)
-            return new_bond, loop_node
-
-        @property
-        def flow(self):
-            return getattr(self, flow_name)
-
-        @flow.setter
-        def flow(self, value):
-            setattr(self, flow_name, value)
-            self.loop_node.loop_into(-value)
-
-    class BondFlow(Bond):
-        """Flow-driven bond."""
-
-        def __init__(self, value: float, loop_node):
-            super().__init__()
-            setattr(self, flow_name, value)
-            setattr(self, effort_name, None)
-            self.loop_node = loop_node
-
-        @classmethod
-        def from_effort(cls, bond):
-            if isinstance(bond, BondEffort):
-                loop_node = vip.loop_node(getattr(bond, effort_name))
-            elif isinstance(bond, Number):
-                loop_node = vip.loop_node(bond)
-            else:
-                raise Exception(f"Incompatible type: {bond} of type {type(bond)}.")
-            new_bond = cls(0, loop_node)
-            return new_bond, loop_node
-
-        @property
-        def effort(self):
-            return getattr(self, effort_name)
-
-        @effort.setter
-        def effort(self, value):
-            setattr(self, effort_name, value)
-            self.loop_node.loop_into(value)
-
-    BondEffort.__name__ = f"{name}Effort"
-    BondFlow.__name__ = f"{name}Flow"
-
-    return BondEffort, BondFlow
+    @Bond.effort.setter
+    def effort(self, value):
+        self.effort = value
+        self.loop_node.loop_into(self.effort)
 
 
-Mechanical1DEffort, Mechanical1DFlow = create_bond_types("Mechanical1D", "force", "velocity")
+class BondEffort(Bond):
+    def __init__(self, value: float, loop_node):
+        super().__init__()
+        self.flow = None
+        self.effort = value
+        self.loop_node = loop_node
+
+    @classmethod
+    def from_flow(cls, bond: "BondFlow"):
+        if isinstance(bond, BondFlow):
+            loop_node = vip.loop_node(bond.flow)
+        elif isinstance(bond, Number):
+            loop_node = vip.loop_node(bond)
+        else:
+            raise Exception(f"Incompatible type: {bond} of type {type(bond)}.")
+        new_bond = cls(0, loop_node)
+        return new_bond, loop_node
+
+    @Bond.flow.setter
+    def flow(self, value):
+        self.flow = value
+        self.loop_node.loop_into(-self.flow)
