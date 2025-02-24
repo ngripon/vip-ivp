13ca902069c410f8ad98f6f74d90712455eb2c76
ngripon <n.gripon@sherpa-eng.com>
2025-02-24 18:14:13 +0100

    pass data

--- visualisation_app/server.py
+++ visualisation_app/server.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, send_from_directory
 
-app = Flask(__name__, static_folder="static", template_folder="static")
+app = Flask(__name__)
+app._static_folder="C:/Users/Nathan/Documents/PetProjects/vip_ivp/visualisation_app/static"
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return send_from_directory("static","index.html")
+
+@app.route("/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
+
+
+@app.route("/api/foo")
+def send_text():
+    return jsonify({"message": "Hey you, out there in the cold!"})
 
 
 if __name__ == "__main__":
