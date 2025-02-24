e77b4f3d97ac2f09b9ad3c2f42c304f8c6ef0a6a
ngripon <n.gripon@sherpa-eng.com>
2025-02-24 11:56:49 +0100

    update loop node logic

--- tests/test_invalid_systems.py
+++ tests/test_invalid_systems.py
@@ -3,10 +3,30 @@ import pytest
 import vip_ivp as vip
 
 
+@pytest.fixture(autouse=True)
+def clear_solver_before_tests():
+    vip.clear()
+
+
 def test_algebraic_loop():
-    x = vip.loop_node(1)
+    x = vip.loop_node()
     ix = vip.integrate(x, 0)
     x.loop_into(x + ix)
 
     with pytest.raises(RecursionError):
+        vip.solve(10)
+
+
+def test_set_loop_node_two_times():
+    x = vip.loop_node()
+    x.loop_into(6)
+    with pytest.raises(Exception):
+        x.loop_into(5)
+
+
+def test_forget_to_set_loop_node():
+    x = vip.loop_node()
+    ix = vip.integrate(x, 0)
+
+    with pytest.raises(Exception):
         vip.solve(10)
