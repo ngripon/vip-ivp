8bd48f7efa73e3dc1f23cb4c11a9593db27464d9
ngripon <n.gripon@sherpa-eng.com>
2025-02-25 10:32:35 +0100

    build the frontend before running the flask server

--- visualisation_app/get_ast.py
+++ visualisation_app/get_ast.py
@@ -1,16 +1,16 @@
 import ast
 
 
-def get_ast(filename:str):
-    with open(filename,"r") as f:
-        src_file=f.read()
+def get_ast(filename: str):
+    with open(filename, "r") as f:
+        src_file = f.read()
 
-    tree=ast.parse(src_file)
+    tree = ast.parse(src_file)
     return tree
 
 
 if __name__ == '__main__':
-    tree=get_ast("../demos/exponential_decay.py")
+    tree = get_ast("../demos/exponential_decay.py")
     print(tree)
     for node in ast.walk(tree):
         print(node)
--- visualisation_app/server.py
+++ visualisation_app/server.py
@@ -1,12 +1,16 @@
+import subprocess
+from pathlib import Path
+
 from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
-app._static_folder="C:/Users/Nathan/Documents/PetProjects/vip_ivp/visualisation_app/static"
+app._static_folder = "C:/Users/Nathan/Documents/PetProjects/vip_ivp/visualisation_app/static"
 
 
 @app.route("/")
 def index():
-    return send_from_directory("static","index.html")
+    return send_from_directory("static", "index.html")
+
 
 @app.route("/<path:path>")
 def serve_static(path):
@@ -19,4 +23,5 @@ def send_text():
 
 
 if __name__ == "__main__":
+    subprocess.call(["npm", "run", "build"], shell=True, cwd="web-app")
     app.run(debug=True)
