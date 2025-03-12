bba0c22fed00b58977fd0e7121e2aef32ca4f7e3
ngripon <nathan.gripon@gmail.com>
2025-03-12 20:40:44 +0100

    reformat code

--- demos/explore_pid_room_temperature.py
+++ demos/explore_pid_room_temperature.py
@@ -3,10 +3,11 @@ import vip_ivp as vip
 
 def regulated_room_temperature(kp=0.1, ki=0.0, kd=0.0):
     temperature_target = 20
-    d_temperature = vip.loop_node()
-    temperature = vip.integrate(d_temperature, 15)
     temperature_ext = 10
     k = 0.005  # Heat transfer coefficient
+
+    d_temperature = vip.loop_node()
+    temperature = vip.integrate(d_temperature, 15)
     error = temperature_target - temperature
     heater_power = kp * error + ki * vip.integrate(error, 0) + kd * vip.differentiate(error)
     d_temperature.loop_into(k * (heater_power - (temperature - temperature_ext)))
