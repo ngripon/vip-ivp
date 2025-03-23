ca8325806c76db5bf1e41d8579edcb1dc03cb5c8
ngripon <nathan.gripon@gmail.com>
2025-03-23 11:37:31 +0100

    add LoopNode object to exposed api

--- src/vip_ivp/__init__.py
+++ src/vip_ivp/__init__.py
@@ -1,2 +1,2 @@
 from .api import create_source, integrate, delay, differentiate, loop_node, solve, explore, clear, new_system, save, \
-    get_var, plot, f, TemporalVar, Solver
+    get_var, plot, f, TemporalVar, Solver, LoopNode
