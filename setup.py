b69668fad55c50154abb4309d94aa9c4060123f3
ngripon <n.gripon@sherpa-eng.com>
2025-03-24 12:08:49 +0100

    add pandas to requirements

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.10",
-    install_requires=['numpy', 'scipy', 'matplotlib']
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pandas']
 )
