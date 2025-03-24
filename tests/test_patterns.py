421e25d5e1ce921a0f212370d79b1b4f3804b6be
ngripon <n.gripon@sherpa-eng.com>
2025-03-24 17:36:18 +0100

    make the unit test

--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -47,12 +47,18 @@ def test_use_numpy_function():
 
     assert np.all(error_array == 0)
 
+
 def test_multidimensional_integration():
     # Source
-    da=vip.create_source(np.array([5,4]))
-    print(da)
-    a=vip.integrate(da,[0,0])
+    da = vip.create_source(np.array([5, 4]))
+    a = vip.integrate(da, [0, 0])
     vip.solve(10)
+
+    a0_fun = lambda t: 5 * t
+    a1_fun = lambda t: 4 * t
+
+    assert np.allclose(a[0].values, a0_fun(a[0].t))
+    assert np.allclose(a[1].values, a1_fun(a[1].t))
 
     # LoopNode
     vip.new_system()
