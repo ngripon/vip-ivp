9644fadab54926a6fa094799d8b81f1d8dc3ab53
ngripon <n.gripon@sherpa-eng.com>
2025-03-26 17:21:38 +0100

    get remaining variables

--- src/vip_ivp/base.py
+++ src/vip_ivp/base.py
@@ -123,6 +123,7 @@ class Solver:
         :param plot: Plot the variables that called the "to_plot()" method.
         :param options: Additional options for the solver. See https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html.
         """
+        self._get_remaining_named_variables()
         # Reinit values
         [var._reset() for var in self.vars]
         start = time.time()
@@ -223,6 +224,18 @@ class Solver:
             return outputs.t, outputs.values
         else:
             return list(map(self.unwrap_leaves, (el for el in outputs)))
+
+    def _get_remaining_named_variables(self):
+        frame = inspect.currentframe().f_back
+        while (frame.f_locals.get("self")
+               and (isinstance(frame.f_locals.get("self"), TemporalVar)
+                    or isinstance(frame.f_locals.get("self"), Solver))
+               or Path(frame.f_code.co_filename).as_posix().endswith("vip_ivp/api.py")):
+            frame = frame.f_back
+        local_variables = frame.f_locals
+        for key, value in local_variables.items():
+            if isinstance(value, TemporalVar) and key not in self.named_vars:
+                self.named_vars[key] = value
 
     def solve_ivp(
             self,
@@ -978,7 +991,7 @@ def get_expression(value) -> str:
         )
         if found_key is not None:
             value.name = found_key
-            value.solver.named_vars[found_key]=value
+            value.solver.named_vars[found_key] = value
             return value.name
         return value.expression
     else:
