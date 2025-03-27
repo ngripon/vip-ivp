4b48be643b23df65745f7793c57864528cb044da
ngripon <n.gripon@sherpa-eng.com>
2025-03-27 15:06:21 +0100

    bugfix: fix error when simulation has no events.

--- src/vip_ivp/base.py
+++ src/vip_ivp/base.py
@@ -380,9 +380,10 @@ class Solver:
         if t_events is not None:
             t_events = np.array([np.asarray(te) for te in t_events])
             y_events = np.array([np.asarray(ye) for ye in y_events])
-            min_t_idx = np.unravel_index(np.argmin(t_events), t_events.shape)
-            self.t.append(t_events[min_t_idx])
-            self.y.append(y_events[min_t_idx])
+            if len(t_events):
+                min_t_idx = np.unravel_index(np.argmin(t_events), t_events.shape)
+                self.t.append(t_events[min_t_idx])
+                self.y.append(y_events[min_t_idx])
 
         if self.t:
             self.t = np.array(self.t)
