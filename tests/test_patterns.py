f0092d1a436d900a46b3c16f27bd5f72241b9fdf
ngripon <n.gripon@sherpa-eng.com>
2025-04-02 16:46:13 +0200

    create failing test for setInterval when there is no integrator

--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -360,7 +360,6 @@ def test_increment_timeout():
 
 def test_increment_interval():
     count=vip.create_source(0)
-    ia=vip.integrate(count,0)
 
     vip.set_interval(count.change_behavior(count+1),2)
 
