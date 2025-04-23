35b9e755d77eadb5dbd1c3aca6cbc51fec4d626f
ngripon <nathan.gripon@gmail.com>
2025-04-23 21:49:57 +0200

    create interactive code block

--- doc/website/src/pages/index.tsx
+++ doc/website/src/pages/index.tsx
@@ -1,17 +1,18 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
+import React, { useEffect, useState } from "react";
+import CodeBlock from "@theme/CodeBlock";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -20,8 +21,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/intro"
+          >
+            Learn
           </Link>
         </div>
       </div>
@@ -30,15 +32,43 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <CodeExample/>
       </main>
     </Layout>
+  );
+}
+
+function CodeExample(): ReactNode {
+  const [k, setk]=useState(0.7)
+
+  return (
+    <div>
+      <label htmlFor="k">Bouncing coefficient</label>
+      <input id="k" type="range" min={0} max={1.5} step={0.01} value={k} onChange={(e)=>setk(e.target.value)} />
+      <CodeBlock language="python">
+        {`k = ${k}  # Bouncing coefficient
+v_min = 0.01  # Minimum velocity need to bounce
+
+# Create the system
+acceleration = vip.temporal(-9.81)
+velocity = vip.integrate(acceleration, x0=0)
+height = vip.integrate(velocity, x0=1)
+
+# Create the bouncing event
+bounce = vip.where(abs(velocity) > v_min, velocity.action_reset_to(-k * velocity), vip.action_terminate)
+height.on_crossing(0, bounce, terminal=False, direction="falling")
+
+# Solve the system
+vip.solve(20, time_step=0.001)`}
+      </CodeBlock>
+    </div>
   );
 }
