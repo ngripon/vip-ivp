e157feb34c5ca4d86b7815748acfa050b102e5e4
ngripon <n.gripon@sherpa-eng.com>
2025-05-13 13:49:19 +0200

    add input maps into clutch examples

--- demos/clutch.py
+++ demos/clutch.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import vip_ivp as vip
 
 # Parameters
@@ -12,7 +13,8 @@ r1, r2 = 1, 1  # radii of plate friction surfaces
 r = 1
 
 locked_w = 1
-unlocked_we = 1
+unlocked_we = vip.temporal(1)
+unlocked_wv = vip.temporal(1)
 
 
 def unlocked_system(t_in, tf_max_k):
@@ -40,13 +42,52 @@ def friction_model(f_n):
     tf_max_k = 2 / 3 * r * f_n * mu_k
     return tf_max_k, tf_max_s
 
-def friction_mode_logic(t_in, tf_max_s):
-    t_f=compute_friction_torque(t_in)
-    torque_condition=np.abs(t_f)<=tf_max_s
 
-    return lock
+def friction_mode_logic(t_in, tf_max_s):
+    t_f = compute_friction_torque(t_in)
+    torque_condition = np.abs(t_f) <= tf_max_s
+    velocities_match = unlocked_we.crosses(unlocked_wv)
+    return velocities_match & torque_condition
 
 
 def compute_friction_torque(t_in):
     t_f = unlocked_we * bv + iv / (ie + iv) * (t_in - unlocked_we * (bv + be))
     return t_f
+
+
+fn_map = np.array([
+    [0, 0],
+    [1, 0.8],
+    [2, 1.6],
+    [3, 1.6],
+    [4, 1.6],
+    [5, 1.6],
+    [6, 0.8],
+    [7, 0],
+    [8, 0],
+    [9, 0],
+    [10, 0]
+])
+
+tn_map = np.array([
+    [1, 2],
+    [2, 2],
+    [3, 2],
+    [4, 2],
+    [5, 2],
+    [6, 0],
+    [7, 0],
+    [8, 0],
+    [9, 0],
+    [10, 0]
+])
+
+
+fn=vip.create_scenario(pd.DataFrame(fn_map),0)[1]
+tn=vip.create_scenario(pd.DataFrame(tn_map),0)[1]
+
+
+fn.to_plot()
+tn.to_plot()
+
+vip.solve(10)
