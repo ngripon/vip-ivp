01615a73b434c5db95cbac7a822e3e94441528e5
ngripon <nathan.gripon@gmail.com>
2026-01-23 17:03:22 +0100

    add recursive collections

--- tests/domain/test_variables.py
+++ tests/domain/test_variables.py
@@ -7,19 +7,29 @@ from src.vip_ivp.domain.variables import TemporalVar
 @pytest.mark.parametrize(
     "test_input,expected",
     [
+        # Scalars
         (5.2, 5.2),
         (None, None),
+        # Collections
         (np.array([1.1, 2, 3]), np.array([1.1, 2, 3])),
         ([1.1, 2, 3], np.array([1.1, 2, 3])),
         ({"a": 1, "b": 2, "c": 3}, {"a": 1, "b": 2, "c": 3}),
+        # Recursive collections
+        (np.arange(6).reshape(2, 3), np.arange(6).reshape(2, 3)),
+        ([[0, 1, 2], [3, 4, 5]], np.arange(6).reshape(2, 3)),
+        (
+                {"a": 1, "b": {"c": 2, "d": 5.5}},
+                {"a": 1, "b": {"c": 2, "d": 5.5}}
+        ),
+        # Function
         (lambda t: 5 * t + 1, 1),
-        (lambda t, y: 5 * t + 1 + y[0]+y[1], 4),
+        (lambda t, y: 5 * t + 1 + y[0] + y[1], 4),
     ]
 )
 def test_output_dimension_with_scalar_time(test_input, expected):
     sut = TemporalVar(test_input)
 
-    output = sut(0, np.array([1,2]))
+    output = sut(0, np.array([1, 2]))
 
     if isinstance(output, np.ndarray):
         np.testing.assert_array_equal(output, expected)
