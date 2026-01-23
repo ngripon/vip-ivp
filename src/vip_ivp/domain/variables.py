80a784d9a3da4600e0d5aecb54957e8b9c0a0226
ngripon <nathan.gripon@gmail.com>
2026-01-23 13:18:43 +0100

    create basic immutable TemporalVar

---
+++ src/vip_ivp/domain/variables.py
@@ -0,0 +1,347 @@
+import enum
+import functools
+import inspect
+import operator
+import time
+import warnings
+from collections import abc
+from copy import copy, deepcopy
+from typing import overload, Literal, Iterable, Dict, Tuple, List, Any
+from pathlib import Path
+from typing import Callable, Union, TypeVar, Generic
+
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
+from typing_extensions import ParamSpec
+
+from ..solver_utils import *
+from ..utils import add_necessary_brackets, convert_to_string, operator_call, shift_array, vectorize_source
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+class CallMode(enum.Enum):
+    CALL_ARGS_FUN = 0
+    CALL_FUN_RESULT = 1
+
+class TemporalVar(Generic[T]):
+    def __init__(
+            self,
+            source: Union[
+                Callable[[Union[float, NDArray], NDArray], T],
+                Callable[[Union[float, NDArray]], T],
+                NDArray,
+                Dict,
+                float,
+                Tuple
+            ] = None,
+            expression: str = None,
+            child_cls=None,
+            operator=None,
+            call_mode: CallMode = CallMode.CALL_ARGS_FUN,
+            is_discrete=False
+    ):
+        self._output_type = None
+        self._is_source = False
+        self._call_mode = call_mode
+        self.is_discrete = is_discrete
+        # Recursive building
+        self.operator = operator
+        child_cls = child_cls or type(self)
+        if self.operator is not None:
+            self.source = source
+        else:
+            self._is_source = True
+            if callable(source) and not isinstance(source, child_cls):
+                n_args = len(inspect.signature(source).parameters)
+                if n_args == 1:
+                    self.source = lambda t, y: vectorize_source(source)(t)
+                else:
+                    self.source = lambda t, y: source(t, y)
+            elif np.isscalar(source):
+                self.source = source
+                self._output_type = type(source)
+            elif isinstance(source, (list, np.ndarray)):
+                self._output_type = np.ndarray
+                self.source = np.vectorize(lambda f: child_cls( f))(
+                    np.array(source)
+                )
+            elif isinstance(source, TemporalVar):
+                vars(self).update(vars(source))
+            elif isinstance(source, dict):
+                self._output_type = dict
+                self.source = {key: child_cls(val) for key, val in source.items()}
+            elif source is None:
+                self.source = None
+            else:
+                raise ValueError(f"Unsupported type: {type(source)}.")
+
+        self._values = None
+        # Variable definition
+        self._expression = convert_to_string(source) if expression is None else expression
+        self.name = None
+
+    def __call__(self, t: Union[float, NDArray], y: NDArray) -> T:
+        # Handle dict in a recursive way
+        if isinstance(self.source, dict):
+            return {key: val(t, y) for key, val in self.source.items()}
+        else:
+            if isinstance(self.source, np.ndarray):
+                if np.isscalar(t):
+                    output = np.stack(np.frompyfunc(lambda f: f(t, y), 1, 1)(self.source))
+                else:
+                    output = np.stack(
+                        np.frompyfunc(lambda f: f(t, y), 1, 1)(self.source.ravel())
+                    ).reshape((*self.source.shape, *np.array(t).shape))
+            elif self.operator is not None:
+                if self.operator is operator_call and not np.isscalar(t):
+                    output = np.array([self._resolve_operator(t[i], y[i]) for i in range(len(t))])
+                    if output.ndim > 1:
+                        output = np.moveaxis(output, 0, -1)
+                else:
+                    output = self._resolve_operator(t, y)
+            else:
+                if callable(self.source):
+                    output = self.source(t, y)
+                elif np.isscalar(t):
+                    output = self.source
+                else:
+                    output = np.full(len(t), self.source)
+        return output
+
+    def _resolve_operator(self, t, y):
+        if self._call_mode == CallMode.CALL_ARGS_FUN:
+            args = [x(t, y) if isinstance(x, TemporalVar) else x for x in self.source if
+                    not isinstance(x, dict)]
+            kwargs = {k: v for d in [x for x in self.source if isinstance(x, dict)] for k, v in d.items()}
+            kwargs = {k: (x(t, y) if isinstance(x, TemporalVar) else x) for k, x in kwargs.items()}
+            return self.operator(*args, **kwargs)
+        elif self._call_mode == CallMode.CALL_FUN_RESULT:
+            args = [x for x in self.source if not isinstance(x, dict)]
+            kwargs = {k: v for d in [x for x in self.source if isinstance(x, dict)] for k, v in d.items()}
+            return self.operator(*args, **kwargs)(t, y)
+        else:
+            raise ValueError(f"Unknown call mode: {self._call_mode}.")
+
+    # @property
+    # def output_type(self):
+    #     if self._output_type is None:
+    #         self._output_type = type(self._first_value())
+    #     return self._output_type
+
+    @classmethod
+    def from_scenario(
+            cls,
+            scenario_table: "pd.DataFrame",
+            time_key: str,
+            interpolation_kind="linear",
+    ) -> "TemporalVar":
+        from scipy.interpolate import interp1d
+
+        variables = {}
+        for col in scenario_table.columns:
+            if col == time_key:
+                continue
+            fun = interp1d(
+                scenario_table[time_key],
+                scenario_table[col],
+                kind=interpolation_kind,
+                bounds_error=False,
+                fill_value=(scenario_table[col].iat[0], scenario_table[col].iat[-1]),
+            )
+            variables[col] = fun
+        return cls(variables)
+
+    def delayed(self, delay: int, initial_value: T = 0) -> "TemporalVar[T]":
+        """
+        Create a delayed version of the TemporalVar.
+        :param delay: Number of solver steps by which the new TemporalVar is delayed.
+        :param initial_value: Value of the delayed variable at the beginning when there is not any value for the original value.
+        :return: Delayed version of the TemporalVar
+        """
+        if delay < 1:
+            raise Exception("Delay accept only a positive step.")
+
+        def create_delay(input_variable):
+            def previous_value(t, y):
+                if np.isscalar(t):
+                    if len(input_variable.solver.t) >= delay:
+                        index = np.searchsorted(input_variable.solver.t, t, "left")
+                        # index = next((i for i, ts in enumerate(input_variable.solver.t) if t <= ts),
+                        #              len(input_variable.solver.t))
+                        if index - delay < 0:
+                            return initial_value
+                        previous_t = input_variable.solver.t[index - delay]
+                        previous_y = input_variable.solver.y[index - delay]
+
+                        return input_variable(previous_t, previous_y)
+                    else:
+                        return initial_value
+                else:
+                    delayed_t = shift_array(t, delay, 0)
+                    delayed_y = shift_array(y, delay, initial_value)
+                    return input_variable(delayed_t, delayed_y)
+
+            return previous_value
+
+        return TemporalVar( (create_delay, self),
+                           expression=f"#DELAY({delay}) {get_expression(self)}",
+                           operator=operator_call,
+                           call_mode=CallMode.CALL_FUN_RESULT,
+                           is_discrete=True)
+
+
+    def m(self, method: Callable[P, T]) -> Callable[P, "TemporalVar[T]"]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> TemporalVar:
+            inputs_expr = [get_expression(inp) if isinstance(inp, TemporalVar) else str(inp) for inp in args]
+            kwargs_expr = [
+                f"{key}={get_expression(value) if isinstance(value, TemporalVar) else str(value)}"
+                for key, value in kwargs.items()
+            ]
+            expression = f"{method.__name__}({', '.join(inputs_expr)}"
+            if kwargs_expr:
+                expression += ", ".join(kwargs_expr)
+            expression += ")"
+            return TemporalVar( (method, self, *args, kwargs),
+                               expression=expression, operator=operator_call)
+
+        functools.update_wrapper(wrapper, method)
+        return wrapper
+
+    def crosses(self, value: Union["TemporalVar[T]", T],
+                direction: Literal["rising", "falling", "both"] = "both") -> "CrossTriggerVar":
+        """
+        Create a signal that triggers when the specified crossing occurs.
+
+        :param value: Value to be crossed to cause the triggering.
+        :param direction: Direction of the crossing.
+        :return: TriggerVar
+        """
+        if self.output_type in (bool, np.bool, str):
+            crossed_variable = self == value
+        elif issubclass(self.output_type, abc.Iterable):
+            raise ValueError(
+                "Can not apply crossing detection to a variable containing a collection of values because it is ambiguous."
+            )
+        else:
+            crossed_variable = self - value
+        expression = f"#CROSSING_BETWEEN {self.name} AND {value.name if isinstance(value, TemporalVar) else value}"
+        trigger_var = CrossTriggerVar(crossed_variable, direction, expression)
+        return trigger_var
+
+
+    @staticmethod
+    def _from_arg(value: Union["TemporalVar[T]", T]) -> "TemporalVar[T]":
+        """
+        Return a TemporalVar from an argument value. If the argument is already a TemporalVar, return it. If not, create a TemporalVar from the value.
+        """
+        if isinstance(value, TemporalVar):
+            return value
+        return TemporalVar(value)
+
+
+    @staticmethod
+    def _apply_logical(logical_fun: Callable, a, b):
+        result = logical_fun(a, b)
+        if result.size == 1:
+            result = result.item()
+        return result
+
+    @staticmethod
+    def _logical_not(a):
+        result = np.logical_not(a)
+        if result.size == 1:
+            result = result.item()
+        return result
+
+    def __invert__(self) -> "TemporalVar[bool]":
+        expression = f"not {add_necessary_brackets(get_expression(self))}"
+        return TemporalVar(
+            (self._logical_not, self),
+            expression,
+            operator=operator_call
+        )
+
+    def __getitem__(self, item):
+        expression = f"{add_necessary_brackets(get_expression(self))}[{item}]"
+        return TemporalVar(
+            (self, item),
+            expression=expression,
+            operator=operator.getitem
+        )
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs) -> "TemporalVar":
+        inputs_expr = [
+            get_expression(inp) if isinstance(inp, TemporalVar) else str(inp)
+            for inp in inputs
+        ]
+        kwargs_expr = [
+            f"{key}={get_expression(value) if isinstance(value, TemporalVar) else str(value)}"
+            for key, value in kwargs.items()
+        ]
+        expression = f"{ufunc.__name__}({', '.join(inputs_expr)}"
+        if kwargs:
+            expression += f", {', '.join(kwargs_expr)}"
+        expression += ")"
+        if method == "__call__":
+            return TemporalVar(
+                (ufunc, *inputs, kwargs),
+                expression=expression,
+                operator=operator_call
+            )
+
+        return NotImplemented
+
+    def __bool__(self):
+        raise ValueError("The truth value of a Temporal Variable is ambiguous. Use vip.where() instead.")
+
+    @property
+    def expression(self):
+        return self._expression
+
+    def __repr__(self) -> str:
+        return f"{self._expression}"
+
+    # NumPy arrays utility methods
+    # @property
+    # def shape(self):
+    #     result = self._first_value()
+    #     if isinstance(result, np.ndarray):
+    #         return result.shape
+    #     raise AttributeError("shape attribute does not exist because this variable does not contain a NumPy array.")
+
+    # Dict utility methods
+    # def keys(self):
+    #     result = self._first_value()
+    #     if isinstance(result, dict):
+    #         return result.keys()
+    #     raise AttributeError("keys() method does not exist because this variable does not contain a dict.")
+    #
+    # def items(self):
+    #     result = self._first_value()
+    #     if isinstance(result, dict):
+    #         key_list = list(result.keys())
+    #         value_list = [self[key] for key in key_list]
+    #         return zip(key_list, value_list)
+    #     raise AttributeError("items() method does not exist because this variable does not contain a dict.")
+
+
+def get_expression(value) -> str:
+    if isinstance(value, TemporalVar):
+        frame = inspect.currentframe().f_back.f_back
+        while (
+                "self" in frame.f_locals
+                and (
+                        isinstance(frame.f_locals["self"], TemporalVar)
+                )
+                or Path(frame.f_code.co_filename).as_posix().endswith("vip_ivp/api.py")
+        ):
+            frame = frame.f_back
+        found_key = next(
+            (key for key, dict_value in frame.f_locals.items() if dict_value is value),
+            None,
+        )
+        return value.expression
+    else:
+        return str(value)
