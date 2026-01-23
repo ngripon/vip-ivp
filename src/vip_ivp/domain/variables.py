96827182ec0aaab33b711d9a57cb8fa9427c3ef8
ngripon <nathan.gripon@gmail.com>
2026-01-23 15:51:10 +0100

    simplify greatly the class

--- src/vip_ivp/domain/variables.py
+++ src/vip_ivp/domain/variables.py
@@ -1,4 +1,3 @@
-import enum
 import functools
 import inspect
 import operator
@@ -10,19 +9,10 @@ import pandas as pd
 from numpy.typing import NDArray
 from typing_extensions import ParamSpec
 
-from .. import TemporalVar
-from ..solver_utils import *
-from ..utils import operator_call, shift_array, vectorize_source
-
 T = TypeVar("T")
 P = ParamSpec("P")
 
 Source = Callable[[float | NDArray, NDArray], T] | Callable[[float | NDArray], T] | NDArray | dict | float
-
-
-class CallMode(enum.Enum):
-    CALL_ARGS_FUN = 0
-    CALL_FUN_RESULT = 1
 
 
 class TemporalVar(Generic[T]):
@@ -30,7 +20,6 @@ class TemporalVar(Generic[T]):
             self,
             source: Source | tuple[Source, ...] = None,
             operator=None,
-            call_mode: CallMode = CallMode.CALL_ARGS_FUN,
             is_discrete=False,
             child_cls=None,
 
@@ -50,7 +39,6 @@ class TemporalVar(Generic[T]):
         :param source: Input from which the internal function is built
         :param child_cls:
         :param operator: If the source input is a tuple, create a function that applies the operator to these source items.
-        :param call_mode:
         :param is_discrete:
         """
         # Object data
@@ -59,7 +47,6 @@ class TemporalVar(Generic[T]):
         self._source = source
         self._operator = operator
 
-        self._call_mode = call_mode
         self._is_discrete = is_discrete
         self._output_type = None
         self._keys: list[str] | None = None
@@ -72,12 +59,23 @@ class TemporalVar(Generic[T]):
             assert type(self._source) is tuple
 
             def operator_func(t, y):
+
+                def resolve_operator(t_inner, y_inner):
+                    """
+                    Compute args and kwargs value and call the operator
+                    """
+                    args = [x(t_inner, y_inner) if isinstance(x, TemporalVar) else x for x in self._source if
+                            not isinstance(x, dict)]
+                    kwargs = {k: v for d in [x for x in self._source if isinstance(x, dict)] for k, v in d.items()}
+                    kwargs = {k: (x(t_inner, y_inner) if isinstance(x, TemporalVar) else x) for k, x in kwargs.items()}
+                    return self._operator(*args, **kwargs)
+
                 if self._operator is operator_call and not np.isscalar(t):
-                    output = np.array([self._resolve_operator(t[i], y[i]) for i in range(len(t))])
+                    output = np.array([resolve_operator(t[i], y[i]) for i in range(len(t))])
                     if output.ndim > 1:
                         output = np.moveaxis(output, 0, -1)
                 else:
-                    output = self._resolve_operator(t, y)
+                    output = resolve_operator(t, y)
                 return output
 
             self.func = operator_func
@@ -89,7 +87,7 @@ class TemporalVar(Generic[T]):
 
                 if n_args == 1:
                     # Function is a temporal function
-                    def temporal_func(t, y):
+                    def temporal_func(t, _):
                         return vectorize_source(self._source)(t)
 
                     self.func = temporal_func
@@ -100,7 +98,7 @@ class TemporalVar(Generic[T]):
                 # Source is a scalar number
                 self._output_type = type(self._source)
 
-                def scalar_func(t, y):
+                def scalar_func(t, _):
                     if np.isscalar(t):
                         return self._source
                     else:
@@ -140,32 +138,12 @@ class TemporalVar(Generic[T]):
     def __call__(self, t: float | NDArray, y: NDArray) -> T:
         return self.func(t, y)
 
-    def _resolve_operator(self, t, y):
-        if self._call_mode == CallMode.CALL_ARGS_FUN:
-            args = [x(t, y) if isinstance(x, TemporalVar) else x for x in self.source if
-                    not isinstance(x, dict)]
-            kwargs = {k: v for d in [x for x in self.source if isinstance(x, dict)] for k, v in d.items()}
-            kwargs = {k: (x(t, y) if isinstance(x, TemporalVar) else x) for k, x in kwargs.items()}
-            return self._operator(*args, **kwargs)
-        elif self._call_mode == CallMode.CALL_FUN_RESULT:
-            args = [x for x in self.source if not isinstance(x, dict)]
-            kwargs = {k: v for d in [x for x in self.source if isinstance(x, dict)] for k, v in d.items()}
-            return self._operator(*args, **kwargs)(t, y)
-        else:
-            raise ValueError(f"Unknown call mode: {self._call_mode}.")
-
-    # @property
-    # def output_type(self):
-    #     if self._output_type is None:
-    #         self._output_type = type(self._first_value())
-    #     return self._output_type
-
     @classmethod
     def from_scenario(
             cls,
-            scenario_table: "pd.DataFrame",
+            scenario_table: pd.DataFrame,
             time_key: str,
-            interpolation_kind="linear",
+            interpolation_kind: str = "linear",
     ) -> "TemporalVar":
         from scipy.interpolate import interp1d
 
@@ -183,78 +161,12 @@ class TemporalVar(Generic[T]):
             variables[col] = fun
         return cls(variables)
 
-    def delayed(self, delay: int, initial_value: T = 0) -> "TemporalVar[T]":
-        """
-        Create a delayed version of the TemporalVar.
-        :param delay: Number of solver steps by which the new TemporalVar is delayed.
-        :param initial_value: Value of the delayed variable at the beginning when there is not any value for the original value.
-        :return: Delayed version of the TemporalVar
-        """
-        if delay < 1:
-            raise Exception("Delay accept only a positive step.")
-
-        def create_delay(input_variable):
-            def previous_value(t, y):
-                if np.isscalar(t):
-                    if len(input_variable.solver.t) >= delay:
-                        index = np.searchsorted(input_variable.solver.t, t, "left")
-                        # index = next((i for i, ts in enumerate(input_variable.solver.t) if t <= ts),
-                        #              len(input_variable.solver.t))
-                        if index - delay < 0:
-                            return initial_value
-                        previous_t = input_variable.solver.t[index - delay]
-                        previous_y = input_variable.solver.y[index - delay]
-
-                        return input_variable(previous_t, previous_y)
-                    else:
-                        return initial_value
-                else:
-                    delayed_t = shift_array(t, delay, 0)
-                    delayed_y = shift_array(y, delay, initial_value)
-                    return input_variable(delayed_t, delayed_y)
-
-            return previous_value
-
-        return TemporalVar((create_delay, self),
-                           operator=operator_call,
-                           call_mode=CallMode.CALL_FUN_RESULT,
-                           is_discrete=True)
-
     def m(self, method: Callable[P, T]) -> Callable[P, "TemporalVar[T]"]:
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> TemporalVar:
             return TemporalVar((method, self, *args, kwargs), operator=operator_call)
 
         functools.update_wrapper(wrapper, method)
         return wrapper
-
-    # def crosses(self, value: "TemporalVar[T]|T",
-    #             direction: Literal["rising", "falling", "both"] = "both") -> "CrossTriggerVar":
-    #     """
-    #     Create a signal that triggers when the specified crossing occurs.
-    #
-    #     :param value: Value to be crossed to cause the triggering.
-    #     :param direction: Direction of the crossing.
-    #     :return: TriggerVar
-    #     """
-    #     if self.output_type in (bool, np.bool, str):
-    #         crossed_variable = self == value
-    #     elif issubclass(self.output_type, abc.Iterable):
-    #         raise ValueError(
-    #             "Can not apply crossing detection to a variable containing a collection of values because it is ambiguous."
-    #         )
-    #     else:
-    #         crossed_variable = self - value
-    #     trigger_var = CrossTriggerVar(crossed_variable, direction)
-    #     return trigger_var
-
-    @staticmethod
-    def _from_arg(value: "TemporalVar[T]|T") -> "TemporalVar[T]":
-        """
-        Return a TemporalVar from an argument value. If the argument is already a TemporalVar, return it. If not, create a TemporalVar from the value.
-        """
-        if isinstance(value, TemporalVar):
-            return value
-        return TemporalVar(value)
 
     @staticmethod
     def _apply_logical(logical_fun: Callable, a, b):
@@ -263,26 +175,14 @@ class TemporalVar(Generic[T]):
             result = result.item()
         return result
 
-    @staticmethod
-    def _logical_not(a):
-        result = np.logical_not(a)
-        if result.size == 1:
-            result = result.item()
-        return result
-
-    def __invert__(self) -> "TemporalVar[bool]":
-        return TemporalVar(
-            (self._logical_not, self),
-            operator=operator_call
-        )
-
     def __getitem__(self, item):
         return TemporalVar(
             (self, item),
             operator=operator.getitem
         )
 
-    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs) -> "TemporalVar":
+    @staticmethod
+    def __array_ufunc__(ufunc, method, *inputs, **kwargs) -> "TemporalVar":
         if method == "__call__":
             return TemporalVar(
                 (ufunc, *inputs, kwargs),
@@ -294,25 +194,77 @@ class TemporalVar(Generic[T]):
     def __bool__(self):
         raise ValueError("The truth value of a Temporal Variable is ambiguous. Use vip.where() instead.")
 
-    # NumPy arrays utility methods
-    # @property
-    # def shape(self):
-    #     result = self._first_value()
-    #     if isinstance(result, np.ndarray):
-    #         return result.shape
-    #     raise AttributeError("shape attribute does not exist because this variable does not contain a NumPy array.")
 
-    # Dict utility methods
-    # def keys(self):
-    #     result = self._first_value()
-    #     if isinstance(result, dict):
-    #         return result.keys()
-    #     raise AttributeError("keys() method does not exist because this variable does not contain a dict.")
-    #
-    # def items(self):
-    #     result = self._first_value()
-    #     if isinstance(result, dict):
-    #         key_list = list(result.keys())
-    #         value_list = [self[key] for key in key_list]
-    #         return zip(key_list, value_list)
-    #     raise AttributeError("items() method does not exist because this variable does not contain a dict.")
+# Utils
+
+def operator_call(obj, /, *args, **kwargs):
+    """operator.call function source code copy in order to be used with Python version <3.11"""
+    return obj(*args, **kwargs)
+
+
+def vectorize_source(fun: Callable) -> Callable:
+    accept_arrays, has_scalar_mode, is_constant = check_if_vectorized(fun)
+
+    def vectorized_wrapper(t):
+        if np.isscalar(t):
+            return fun(t)
+        else:
+            return np.array([fun(ti) for ti in t])
+
+    def handle_scalar_wrapper(t):
+        output = fun(t)
+        if np.isscalar(t):
+            return output[0]
+        return output
+
+    def constant_wrapper(t):
+        if np.isscalar(t):
+            return fun(t)
+        else:
+            value = fun(0)
+            if np.isscalar(value):
+                return np.full(len(t), value)
+            else:
+                return np.moveaxis(np.broadcast_to(value, (len(t),) + value.shape), 0, -1)
+
+    if is_constant:
+        return constant_wrapper
+    elif accept_arrays and has_scalar_mode:
+        return fun
+    elif not has_scalar_mode:
+        return handle_scalar_wrapper
+    else:
+        print(f"Warning: the function '{fun.__name__}' is not vectorizable.")
+        return vectorized_wrapper
+
+
+def check_if_vectorized(fun) -> tuple[bool, bool, bool]:
+    accept_arrays = True
+    has_scalar_mode = True
+    is_constant = False
+
+    # Test with scalar
+    scalar_output = fun(0)
+
+    # Find a t length that does not match the len of a dimension of a scalar output
+    array_len = 3
+    if not np.isscalar(scalar_output):
+        while array_len in scalar_output.shape:
+            array_len += 1
+
+    # Test with array input
+    array_input = np.zeros(array_len)
+    try:
+        array_output = fun(array_input)
+        if np.isscalar(array_output) or not np.isscalar(scalar_output) and array_output.shape == scalar_output.shape:
+            is_constant = True
+            raise ValueError("The array output does not create a vector")
+        scalar_ndim = 0 if np.isscalar(scalar_output) else np.asarray(scalar_output).ndim
+        if array_output.ndim == scalar_ndim:
+            has_scalar_mode = False
+        elif scalar_ndim != array_output.ndim - 1:
+            raise ValueError("There is something wrong in the output dimensions of the function.")
+    except ValueError:
+        accept_arrays = False
+
+    return accept_arrays, has_scalar_mode, is_constant
