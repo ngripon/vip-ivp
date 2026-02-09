65f32ec3c6733677e68a3b1ee8e6768a40fbb404
ngripon <nathan.gripon@gmail.com>
2026-02-09 22:32:54 +0100

    refactor ballistics.py

--- demos/ballistics.py
+++ demos/ballistics.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import vip_ivp as vip
 import numpy as np
 
-# TODO: Refactor when multidimensional states will be available
-
 # Parameters
 GRAVITY = -9.81
 v0 = 30
@@ -15,20 +13,20 @@ v0 = [v0 * np.cos(th0), v0 * np.sin(th0)]
 x0 = [0, 0]
 
 # Create the system
-acceleration = vip.loop_node(2)
-velocity = vip.integrate(acceleration, v0)
-position = vip.integrate(velocity, x0)
+velocity = [vip.state(x) for x in v0]
+position = [vip.state(x, derivative=v) for x, v in zip(x0, velocity)]
 v_norm = np.sqrt(velocity[0] ** 2 + velocity[1] ** 2)
-acceleration.loop_into([-mu * velocity[0] * v_norm,
-                        GRAVITY - mu * velocity[1] * v_norm])
+velocity[0].der = -mu * velocity[0] * v_norm
+velocity[1].der = GRAVITY - mu * velocity[1] * v_norm
+
 # Terminate on touching the ground
 hit_ground = position[1].crosses(0, "falling")
-vip.terminate_on(hit_ground)
+vip.when(hit_ground, vip.terminate)
 
-vip.solve(10, time_step=0.01)
+vip.solve(10)
 
 # Plot results
-x, y = position.values
+x, y = [x.values for x in position]
 plt.plot(x, y)
 plt.title("Projectile motion")
 plt.xlabel("X (m)")
