3d408a0af95d7fc6a19a2c353567fa58e45a9f3a
ngripon <nathan.gripon@gmail.com>
2026-02-11 23:26:41 +0100

    document strategy

--- src/vip_ivp/application_services/variable_expressions.py
+++ src/vip_ivp/application_services/variable_expressions.py
@@ -1,3 +1,26 @@
+"""
+The goal of variable expression is to be able to reconstruct the way the system is built in the code.
+
+The systems are flat: if a variable is created in a function, it has no way to know that. Expression data aim to fill
+this knowledge gap.
+
+Variable expression stores 2 information:
+    - Declaration data : in which function frame the variable has been created, and its expression in it.
+    - Name data : which name has the variable in each frame.
+
+The ultimate goal is to reconstruct the system graph. To build the system graph:
+    1. Named variables must be stored.
+    2. Pick a variable and explore its sources:
+        a. In the creation frame, draw a block containing the expression.
+        b. The block inputs are the first named variables attained while searching the sources.
+        c. If there is a frame change on the chain:
+            - If the frame is deeper, on the current level the variable is coming out of the function of the frame. In
+            the frame, the current variable is an output.
+            - If the frame is shallower, on the current level the variable is coming from an input. In the frame, the
+            current variable goes into an input of the function that was on the current level.
+"""
+
+
 import inspect
 from pathlib import Path
 from types import FrameType
